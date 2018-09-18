9bb824ff6909920a105455fbced48446ac90802c
takumigashira <takumigashira@gmail.com>
2018-09-19 05:58:34 +0900

    add '+' '-'

--- test.sh
+++ test.sh
@@ -6,11 +6,16 @@ try() {
   gcc -static -o tmp tmp.s
   ./tmp
   actual="$?"
-  if [ "$actual" != "$expected" ]; then
+  if [ "$actual" == "$expected" ]; then
+   echo "$input => $actual" 
+  else
     echo "$input expected, but got $actual"
     exit 1
   fi
 }
 try 0 0
 try 42 42
+try 21 '5+20-4'
+
 echo OK
+
