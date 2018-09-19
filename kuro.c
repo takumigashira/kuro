800127d9ee054ae6f2724f4b7f385af09c8cb34f
takumigashira <takumigashira@gmail.com>
2018-09-20 08:53:15 +0900

    add tokenize

--- kuro.c
+++ kuro.c
@@ -1,5 +1,57 @@
+#include<ctype.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+enum {
+	TK_NUM = 256,
+	TK_EOF,
+};
+
+//token type
+typedef struct {
+	int ty;
+	int val;
+	char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p){
+	int i = 0;
+	while(*p){
+		//skip whitespace
+		if(isspace(*p)){
+			p++;
+			continue;
+		}
+
+		// + or -
+		if(*p == '+' || *p == '-'){
+			tokens[i].ty = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+			continue;
+		}
+		//number
+		if(isdigit(*p)){
+			tokens[i].ty = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].val = strtol(p,&p,10);
+			i++;
+			continue;
+		}
+		fprintf(stderr, "can not tokenize %s", p);
+		exit(1);
+	}
+	tokens[i].ty = TK_EOF;
+}
+
+void fail(int i){
+	fprintf(stderr, "unexpected token: %s\n", tokens[i].input);
+	exit(1);
+}
 
 int main(int argc,char **argv){
    if(argc != 2){
@@ -8,27 +60,35 @@ int main(int argc,char **argv){
    }
 
    char *p = argv[1];
+   tokenize(argv[1]);
 
    printf(".intel_syntax noprefix\n");
    printf(".global main\n");
    printf("main:\n");
-   printf("   mov rax, %ld\n",strtol(p, &p, 10));
-
-   while (*p) {
-     if(*p == '+'){ 
-       p++;
-       printf("   add rax, %ld\n",strtol(p, &p ,10)); 
-       continue;
-     }
-
-     if(*p == '-'){ 
-       p++;
-       printf("   sub rax, %ld\n",strtol(p, &p, 10)); 
-       continue;
-     }
-
-     fprintf(stderr, "unexpected character: %c\n", *p);
-     return 1;
+   
+   if(tokens[0].ty != TK_NUM)
+	   fail(0);
+   printf("   mov rax, %d\n", tokens[0].val);
+   
+   int i = 1;
+   while(tokens[i].ty != TK_EOF){
+	   if(tokens[i].ty == '+'){
+		   i++;
+		   if(tokens[i].ty != TK_NUM)
+			   fail(i);
+		   printf("   add rax, %d\n", tokens[i].val);
+		   i++;
+		   continue;
+	   }
+	   if(tokens[i].ty == '-'){
+		   i++;
+		   if(tokens[i].ty != TK_NUM)
+			   fail(1);
+		   printf("   sub rax, %d\n", tokens[i].val);
+		   i++;
+		   continue;
+	   }
+	   fail(1);
    }
 
    printf("   ret\n");
